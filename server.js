738f93d75bb2cf91e22d8cd3ac50be5c8ebad294
Omar <o.moreta@gmail.com>
2022-03-01 23:10:52 -0500

    added stages controller and routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const { Sequelize } = require("sequelize");
 const bandsController = require("./controllers/bands_controller");
 const eventsController = require("./controllers/events_controller");
+const stagesController = require("./controllers/stages_controller");
 
 // CONFIGURATION / MIDDLEWARE
 require("dotenv").config();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/bands", bandsController);
 app.use("/events", eventsController);
+app.use("/stages", stagesController);
 
 // ROOT
 app.get("/", (req, res) => {
